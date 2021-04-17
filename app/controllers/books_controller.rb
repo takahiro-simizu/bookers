83cc9011196b443b54307cbdaade68af634698f8
takahiro-simizu <tkbchando@gmail.com>
2021-04-17 09:17:01 +0000

    [Update]indexページのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def index
     @book = Book.new
     @books = Book.all
@@ -18,16 +14,21 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
-    
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
